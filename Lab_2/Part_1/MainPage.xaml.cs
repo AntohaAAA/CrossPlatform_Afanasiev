e38373367d4a5b71d068ac14e1c7a3d5a43f4734
AntohaAAA <81568235+AntohaAAA@users.noreply.github.com>
2023-06-06 10:29:19 +0300

    Add files via upload

---
+++ Lab_2/Part_1/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        private ObservableCollection<string> items;
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+            items = new ObservableCollection<string>();
+            listView.ItemsSource = items;
+            string text = "AAAAA";
+            items.Add(text);
+        }
+
+        private void OnAddClicked(object sender, EventArgs e)
+        {
+            string text = entry.Text;
+            if (!string.IsNullOrEmpty(text))
+            {
+                items.Add(text);
+                entry.Text = string.Empty;
+            }
+        }
+
+        private void OnDeleteClicked(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var item = button.BindingContext as string;
+            items.Remove(item);
+        }
+
+        private void OnItemSelected(object sender, EventArgs e)
+        {
+            var item = (sender as ListView).SelectedItem as string;
+            labelSelectedItem.Text = item;
+        }
+    }
+}
+
---
+++ Lab_2/Part_1/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
